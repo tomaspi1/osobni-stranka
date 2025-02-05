d03eacbc578ff128b49ba2eb5f1237592fa0972d
tomaspi1 <108697855+tomaspi1@users.noreply.github.com>
2025-02-05 03:47:59 +0100

    Update seznam.js

--- seznam.js
+++ seznam.js
@@ -1,25 +1,25 @@
 const projects = [
-    { name: "Kalkulačka výdělku", link: "https://jellyfish-app-99lni.ondigitalocean.app/", icon: "code" },
-    { name: "Hra Snake", link: "http://had.tomaspitro.cz/", icon: "rocket" },
-    { name: "Osobní stránka- tomaspitro.cz", link: "index.html", icon: "book" },
-    { name: "TPMM-eshop-mobil", link: "https://tp-mobil.000webhostapp.com/", icon: "globe" },
-    { name: "Další projekty a všechny repozitáře", link: "https://github.com/tomaspi1?tab=repositories", icon: "star" }
+  { name: "Hra Snake (JS)", link: "http://had.xdtisk.cz/", icon: "rocket" },
+  { name: "Osobní stránka- tomaspitro.cz", link: "index.html", icon: "book" },
+ // { name: "Nový projekt", link: "https://ankh-sepia.vercel.app/", icon: "globe" },
+{ name: "Starý Egypt (REACT)", link: "https://staryegypt.vercel.app/", icon: "eye" },
+  { name: "Další projekty a všechny repozitáře", link: "https://github.com/tomaspi1?tab=repositories", icon: "star" }
 ];
 
 const projectList = document.getElementById("project-list");
 
 projects.forEach(project => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.href = project.link;
-    link.target = "_blank";
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = project.link;
+  link.target = "_blank";
 
-    const icon = document.createElement("i");
-    icon.className = `fas fa-${project.icon}`;
+  const icon = document.createElement("i");
+  icon.className = `fas fa-${project.icon}`;
 
-    link.appendChild(icon);
-    link.innerHTML += project.name;
+  link.appendChild(icon);
+  link.innerHTML += project.name;
 
-    listItem.appendChild(link);
-    projectList.appendChild(listItem);
+  listItem.appendChild(link);
+  projectList.appendChild(listItem);
 });
