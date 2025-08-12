be779778c62cc4de9c3243b3b9801ac745a1f9f3
tomaspi1 <108697855+tomaspi1@users.noreply.github.com>
2025-08-13 01:29:38 +0200

    Update seznam.js

--- seznam.js
+++ seznam.js
@@ -1,9 +1,9 @@
 const projects = [
-  // { name: "Hra Snake (JS)", link: "http://had.xdtisk.cz/", icon: "rocket" },
-  { name: "Osobní stránka- tomaspitro.cz", link: "index.html", icon: "book" },
- // { name: "Nový projekt", link: "https://ankh-sepia.vercel.app/", icon: "globe" },
-{ name: "Starý Egypt (REACT)", link: "https://staryegypt.vercel.app/", icon: "eye" },
-  { name: "Další projekty a všechny repozitáře", link: "https://github.com/tomaspi1?tab=repositories", icon: "star" }
+  { name: "Blog - Domácí bylinky (Elementor)", link: "https://elementor2.tomaspitro.cz/", icon: "leaf" },         // bylinky = listí, příroda, čerstvost
+  { name: "E-shop- Bylinky (Woocommerce, Elementor)", link: "https://woocommerce-tomaspitro-cz.webkitty.online/", icon: "shopping-cart" }, // eshop = košík, jasná věc
+  // { name: "Nový projekt", link: "https://ankh-sepia.vercel.app/", icon: "globe" },
+  { name: "Blog - Starý Egypt (REACT)", link: "https://staryegypt.vercel.app/", icon: "eye" },                   // oko Horovo, mystika Egypta
+  { name: "Další projekty a všechny repozitáře", link: "https://github.com/tomaspi1?tab=repositories", icon: "code" }  // repozitáře = kód
 ];
 
 const projectList = document.getElementById("project-list");
