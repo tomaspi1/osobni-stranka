19edfc39f8eb61de709f13b7e1a69f97dcfff731
tomaspi1 <108697855+tomaspi1@users.noreply.github.com>
2025-08-13 02:46:31 +0200

    Update seznam.js

--- seznam.js
+++ seznam.js
@@ -2,7 +2,7 @@ const projects = [
   { name: "Blog - Domácí bylinky (Elementor)", link: "https://elementor2.tomaspitro.cz/", icon: "leaf" },        
   { name: "E-shop- Bylinky (Woocommerce, Elementor)", link: "https://woocommerce-tomaspitro-cz.webkitty.online/", icon: "shopping-cart" }, 
   { name: "Blog - Starý Egypt (REACT)", link: "https://staryegypt.vercel.app/", icon: "eye" },                    
-  { name: "E-shop - 3D tisk( Wordpress - testovací verze)", link: "https://projekt-wp1.vercel.app/", icon: "cogwheel" },
+  { name: "E-shop - 3D tisk( Wordpress - testovací verze)", link: "https://projekt-wp1.vercel.app/", icon: "wrench" },
   { name: "Další projekty a všechny repozitáře", link: "https://github.com/tomaspi1?tab=repositories", icon: "code" }  // repozitáře = kód
 ];
 
